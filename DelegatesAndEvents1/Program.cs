30961a014f1bd4f10241077529754e113caa8117
saksham duseja <sakshamduseja222@gmail.com>
2019-03-08 18:07:17 -0500

    lab 1

--- DelegatesAndEvents1/Program.cs
+++ DelegatesAndEvents1/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents1
 {
+    // Saksham Saksham (C0732116)
+    // CSD3354 Section 2
+    // Assignment 2
+    // March 8, 2019
     class Program
     {
         static void Main(string[] args)
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
         }
     }
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+
+    }
 }
+
