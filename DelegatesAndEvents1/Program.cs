22187916461d7259c8176b18f36772d2909ebbf0
saksham duseja <sakshamduseja222@gmail.com>
2019-03-08 20:00:10 -0500

    lab 6

--- DelegatesAndEvents1/Program.cs
+++ DelegatesAndEvents1/Program.cs
@@ -16,25 +16,37 @@ namespace DelegatesAndEvents1
         {
             DelegateExercises delegateExercises = new DelegateExercises();
             delegateExercises.Method3();
+            Console.ReadLine();
         }
+    }
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate(int intValue);
 
-        public class DelegateExercises
+        int Method1(int intMethod1)
         {
-            public delegate int MyDelegate();
-
-            void Method1()
-            {
-                System.Console.WriteLine("MyDelegate");
-            }
-
-            public void Method3()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                myDelegate();
-            }
+            return intMethod1 * 2;
         }
 
+        int Method2(int intMethod1)
+        {
+            return intMethod1 * 10;
+        }
 
+        public void Method4(MyDelegate myDelegate)
+        {
+            int result = myDelegate(10);
+            Console.WriteLine(result);
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
+        }
     }
 }
+
 
