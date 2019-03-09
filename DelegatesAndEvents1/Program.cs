337d07b4af9242d7b0956ff0bd4d65a088a7ac9d
saksham duseja <sakshamduseja222@gmail.com>
2019-03-08 21:24:38 -0500

    lab 14

--- DelegatesAndEvents1/Program.cs
+++ DelegatesAndEvents1/Program.cs
@@ -12,40 +12,32 @@ namespace DelegatesAndEvents1
     // CSD3354 Section 2
     // Assignment 2
     // March 8, 2019
-    namespace DelegatesAndEvents
+    public class Program
     {
-
-
-        public class Program
+        public static void Main()
         {
-            public static void Main()
-            {
-                DelegateExercises delegateExercises = new DelegateExercises();
-                delegateExercises.Method3();
-                Console.ReadLine();
-            }
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
         }
+    }
+    public delegate int MyDelegate(out int i);
 
-        public delegate int MyDelegate(out int i);
-
-        public class DelegateExercises
+    public class DelegateExercises
+    {
+        int Method1(out int i)
         {
-            int Method1(out int i)
-            {
-                System.Console.WriteLine("Method1");
-                i = 10;
-                return 0;
-            }
-
-            public void Method3()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                MyDelegate myDelegate1 = new MyDelegate(Method1);
-                MyDelegate myDelegate2 = myDelegate + myDelegate1;
-                int intValue;
-                myDelegate2(out intValue);
-
-            }
+            i = 100;
+            System.Console.WriteLine("Method1 " + i);
+            return 0;
+        }
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            MyDelegate myDelegate1 = null;
+            MyDelegate myDelegate2 = myDelegate + myDelegate1;
+            int intValue;
+            myDelegate2(out intValue);
         }
     }
 }
