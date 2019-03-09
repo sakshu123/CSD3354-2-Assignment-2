4e239b9166b0068633976175b67fb55b5831b39d
saksham duseja <sakshamduseja222@gmail.com>
2019-03-08 20:57:58 -0500

    lab 10

--- DelegatesAndEvents1/Program.cs
+++ DelegatesAndEvents1/Program.cs
@@ -12,9 +12,33 @@ namespace DelegatesAndEvents1
     // CSD3354 Section 2
     // Assignment 2
     // March 8, 2019
-    public delegate void MyDelegate();
-    public class DelegateExercises : MyDelegate
+    public class Program
     {
+        public static void Main()
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
+        }
+    }
+
+    public delegate void MyDelegate();
+
+    public class DelegateExercises
+    {
+        void Method1()
+        {
+            System.Console.WriteLine("Method1");
+
+
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+            System.Console.WriteLine(myDelegate.ToString());
+        }
     }
 }
 
