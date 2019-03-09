8c198a31a8ca50fe9298ef47eb8a242b58ac38c0
saksham duseja <sakshamduseja222@gmail.com>
2019-03-08 20:17:07 -0500

    lab 7

--- DelegatesAndEvents1/Program.cs
+++ DelegatesAndEvents1/Program.cs
@@ -10,33 +10,53 @@ namespace DelegatesAndEvents1
     // CSD3354 Section 2
     // Assignment 2
     // March 8, 2019
-    public class Program
-    {
-        public static void Main()
-        {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
-        }
-    }
     public class DelegateExercises
     {
         public delegate int MyDelegate(int intValue);
 
         int Method1(int intMethod1)
         {
-            return intMethod1 * 2;
+            return intMethod1 * 4;
         }
 
         int Method2(int intMethod1)
         {
-            return intMethod1 * 10;
+            return intMethod1 * 20;
         }
 
         public void Method4(MyDelegate myDelegate)
         {
-            int result = myDelegate(10);
-            Console.WriteLine(result);
+            for (int i = 1; i <= 5; i++)
+                System.Console.Write(myDelegate(i) + " ");
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
+        }
+    }
+}
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate(int intValue);
+
+        int Method1(int intMethod1)
+        {
+            return intMethod1 * 4;
+        }
+
+        int Method2(int intMethod1)
+        {
+            return intMethod1 * 20;
+        }
+
+        public void Method4(MyDelegate myDelegate)
+        {
+            for (int i = 1; i <= 5; i++)
+                System.Console.Write(myDelegate(i) + " ");
         }
 
         public void Method3()
